37e1e8a976a33b4253771666d62430d5086e1236
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-03-31 21:10:50 +0530

    cobra scripts

--- Git/git_install.py
+++ Git/git_install.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
+import os
+print("Your System is updating!..")
+os.system('sudo apt update')
+print("Your System is upgrading!..")
+#os.system('sudo apt upgrade')
+print("Installing git..")
+os.system('sudo apt install git')
+print("Check git version!..")
+os.system('git --version')
+print("Git is Installed Successfully.")
 
