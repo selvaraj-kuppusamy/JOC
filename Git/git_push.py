cbc15a46ac37b26674df24e386fd1a26590a7c9b
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-04-12 21:24:27 +0530

    python scripts

--- Git/git_push.py
+++ Git/git_push.py
@@ -1,7 +1,10 @@
 #!/bin/usr/py
 import os
+print("Checking git status")
 os.system('git status')
+print("Add git status")
 os.system('git add .')
+print("Again add git status")
 os.system('git status')
+print("git commiting")
 os.system('git commit -m "python scripts"')
-
